34bb1deffec20970a6ce17d87b1fac0e4d0b741f
xmadmaxdx <madmaxsecondac@gmail.com>
2026-09-02 15:21:09 +0600

    Data types all of them

---
+++ 07_Data_Types/dataTypes.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int myNum = 5;
+    float myFloat = 5.99;
+    double myDouble = 9.98;
+    char myLetter = 'D';
+
+    printf("%d\n", myNum);
+    printf("%f\n", myFloat);
+    printf("%lf\n", myDouble);
+    printf("%c\n", myLetter);
+    return 0;
+}
