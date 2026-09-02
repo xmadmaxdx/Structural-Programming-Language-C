bea8b67919d4d64be6fe32ea2956e92aa01b98c1
xmadmaxdx <madmaxsecondac@gmail.com>
2026-09-02 15:20:59 +0600

    Comments demo done

---
+++ 05_Comments/commentDemo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// This is a single line comment
+
+int main() {
+    int a = 5;
+    int b = 10;
+
+    /*
+        This is a multi-line comment
+        It can span multiple lines
+    */
+
+    printf("%d\n", a + b);
+    return 0;
+}
