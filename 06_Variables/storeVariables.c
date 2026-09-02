2a8987027d510fd48c07dd95fda5cda8365791da
xmadmaxdx <madmaxsecondac@gmail.com>
2026-09-02 15:21:06 +0600

    Variables storing and printing

---
+++ 06_Variables/storeVariables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int myNum = 15;
+    char myLetter = 'D';
+    float price = 5.99;
+    char myText[] = "Hello";
+
+    printf("%d\n", myNum);
+    printf("%c\n", myLetter);
+    printf("%.2f\n", price);
+    printf("%s\n", myText);
+    return 0;
+}
