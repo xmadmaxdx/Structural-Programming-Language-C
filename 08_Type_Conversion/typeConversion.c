95a2d2c58e13b98207863732f957e9c2a87aa0d8
xmadmaxdx <madmaxsecondac@gmail.com>
2026-09-03 16:35:54 +0600

    Conversion and casting

---
+++ 08_Type_Conversion/typeConversion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x = 5;
+    int y = 2;
+
+    printf("%d\n", x / y);
+    printf("%.1f\n", (float) x / y);
+    printf("%d\n", (int) 4.5);
+    return 0;
+}
