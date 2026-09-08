c7909e3e519d8708c6834e7ee16cd9bcc3f29ab0
xmadmaxdx <madmaxsecondac@gmail.com>
2026-09-08 09:55:35 +0600

    Reverse digits while loop

---
+++ assignment1/15_While_Loop/reverseNumber.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num, reverse = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num > 0) {
+        reverse = reverse * 10 + num % 10;
+        num /= 10;
+    }
+
+    printf("Reversed: %d\n", reverse);
+    return 0;
+}
