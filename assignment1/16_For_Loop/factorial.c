70795710beec9f4b08fcfca2ecd891b20e18a98c
xmadmaxdx <madmaxsecondac@gmail.com>
2026-09-10 13:05:41 +0600

    Factorial of a number

---
+++ assignment1/16_For_Loop/factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    printf("Factorial = %d\n", fact);
+    return 0;
+}
