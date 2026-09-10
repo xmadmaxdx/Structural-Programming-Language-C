576368af608701da5d81659dffc2d90cf7fd31e8
xmadmaxdx <madmaxsecondac@gmail.com>
2026-09-10 13:05:57 +0600

    Sum with for loop

---
+++ assignment1/16_For_Loop/sumOfNumbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+
+    printf("Sum = %d\n", sum);
+    return 0;
+}
